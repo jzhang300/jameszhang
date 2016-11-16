900028ba4042114d23eea2cd232e02e94ae25fdc
James Zhang <james.zhang75024@gmail.com>
2016-11-15 23:01:05 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,138 @@
+var express = require('express'),
+    path = require('path'),
+    os = require('os'),
+    logger = require('morgan'),
+    errorHandler = require('errorhandler'),
+    markdownServe = require('markdown-serve'),
+    wrench = require('wrench'),
+    Promise = require('bluebird'),
+    parser = require('./node_modules/markdown-serve/lib/parser.js'),
+    resolver = require('./node_modules/markdown-serve/lib/resolver.js');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+  app.set('host', 'http://localhost');
+}
+
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+// Without view option specified, data is returned as JSON.
+// Here we are mounting on a separate sub-path... so to get at the about page for example, we would access http://localhost:3000/json/about
+// NOTE that the order of middleware(s) are important, so we need to put this first to route any requests to "/json" and handle as putting
+// this middleware after will never invoke it
+app.use('/json', markdownServe.middleware({
+  rootDirectory: path.resolve(__dirname, 'posts'),
+}));
+
+app.get('/pages', function(req, res, next) {
+  getPages(req, res, next);
+});
+
+app.get('/pages/:directory', function(req, res, next) {
+  getPages(req, res, next, req.params.directory);
+});
+
+app.use(markdownServe.middleware({
+  rootDirectory: path.resolve(__dirname, 'posts'),
+  view: 'markdown'
+}));
+
+app.listen(app.get('port'), function() {
+  var h = (app.get('host') || os.hostname() || 'unknown') + ':' + app.get('port');
+  console.log('Express server listening at ' + h);
+});
+
+/*
+Get Pages
+*/
+function getPages(req, res, next, directoryPath) {
+  // initiate promise
+  var files = (function() {
+    return Promise.resolve([]);
+  })();
+
+  // gets all filenames
+  function getAllFilenames(results) {
+    // readdirRecursive returns null as last element to indicate it's done reading all files.
+    var hasTerminated = false;
+
+    return new Promise(function(resolve, reject) {
+      wrench.readdirRecursive('posts', function(error, curFiles) {
+        // add new filenames to array and filter out directory names
+        results = results.concat(curFiles).filter(function(value) {
+          if (value !== null) {
+            return value.includes('.md') ? true : false;
+          } else {
+            hasTerminated = true;
+            return false;
+          }
+        });
+
+        // if wrench function has looked through all subdirectory filenames, resolve
+        if (hasTerminated)
+          resolve(results);
+      });
+    });
+  };
+
+  // filter only files of directoryPath
+  function filterDirectoryFiles(results) {
+    if (typeof directoryPath !== 'undefined') {
+      var re = new RegExp('^' + directoryPath + '/');
+      return results.filter(function(filename) {
+        return re.test(filename);
+      });
+    }
+    else
+      return results;
+  }
+
+  // get markdown data
+  function parseFiles(results) {
+    // array of promises that get markdown metadata
+    var newResults = [];
+    results.forEach(function(filename) {
+      newResults.push(
+        new Promise(function(resolve, reject) {
+          parser.parse(path.join(__dirname + '/posts/', filename), {}, function(err, result) {
+            if (err)
+              reject(err);
+            else {
+              resolve({
+                uriPath: filename.replace('.md', ''),
+                meta: result.meta
+              });
+            }
+          })
+        })
+      );
+    });
+
+    return Promise.all(newResults);
+  }
+    // return results.map(function(filename) {
+    //
+    // });
+
+
+  files.then(getAllFilenames)
+       .then(filterDirectoryFiles)
+       .then(parseFiles)
+       .then(function(results) {
+         res.json(results);
+       }).catch(function(error) {
+         next(error);
+       });
+}
---
+++ posts/articles/sample.md
@@ -0,0 +1,23 @@
+title: Sample page
+view: custom
+published: 2014-04-08
+---
+# Sample Text: The Best Color Scheme for Your Website
+
+#### The majority of websites are composed of a bright, usually white background and dark text. Then there's the small minority of the web: dark websites, colorful websites. Why is the bright background used by the majority of websites?
+
+## Utilitarian Motivation
+
+The common use for small caps is for abbreviations longer than 2 letters, such as css, html and wysiwyg excluding am, pm, bc and similar.
+
+Research shows that consumers with a utilitarian motivation find a low-arousal environment more pleasurable than a high arousal one. The degree of arousal refers to how exciting the environment is to our senses. In simpler English, users motivated by usefulness want a non-exciting environment.
+
+### The Research
+
+Research shows that consumers with a utilitarian motivation find a low-arousal environment more pleasurable than a high arousal one. The degree of arousal refers to how exciting the environment is to our senses. In simpler English, users motivated by usefulness want a non-exciting environment.
+
+## Hedonistic Motivation
+
+Hedonism is defined as the pursue of pleasure, especially the pleasure of our senses. Research shows that consumers with hedonistic motivations find a high-arousal environment more pleasurable than a low-arousal one. In simple English, users motivated by pleasure prefer an exciting environment.
+
+Example outside the web: nightclubs. Colorful lights flash, music pounds and beverages satisfy your taste buds. It is truly a high-
